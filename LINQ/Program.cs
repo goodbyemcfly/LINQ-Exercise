084cba982e40b029e5cf8c361311a49fc7282ecd
Brett Bellomy <brettbellomymusic@gmail.com>
2024-05-19 14:34:20 -0500

    first commit: Completed assignment

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace LINQ
+﻿using Microsoft.VisualBasic;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>
+            {"The Last of Us pt II",
+             "The Last of Us",
+             "The Legend of Zelda: Tears of the Kingdom",
+             "Neir: Automata",
+             "The Ghost of Tsushima",
+            };
+
+            Console.WriteLine("My top 5 favorite video games");
+            Console.WriteLine("_______________________________");
+
+            foreach (var game in videoGames)
+            {
+                Console.WriteLine(game);
+            }
+
+            Console.WriteLine("My top 5 favorite video games (sorted by length)");
+            Console.WriteLine("_______________________________");
+
+            var gamesByLength = videoGames.OrderBy(game => game.Length).ToList();
+
+            foreach (var game in gamesByLength)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
